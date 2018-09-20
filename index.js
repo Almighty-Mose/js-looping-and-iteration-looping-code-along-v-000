2ca7e1523881910edd599f68b603590843abe46f
Branden Mosley <branden.mosley1337@gmail.com>
2018-09-20 04:23:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solutions in this file
-function tailsNeverFails() {
-  while
+function printBadges(names) {
+  for(let i = 0; i < names.length; i++) {
+    console.log(`Welcome, ${name}! You are employee #${i}.`);
+  }
+  return names;
 }
